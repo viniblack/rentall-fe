2f7f619aa00cb11f2e21c2722add6f3e28f90914
Vini Black <viniibl4ck@gmail.com>
2025-04-03 15:35:18 -0300

    Config eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,23 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends(
+    "@rocketseat/eslint-config/next",
+    "next/core-web-vitals"
+  ),
+  {
+    plugins: {
+      "simple-import-sort": require("eslint-plugin-simple-import-sort"),
+      "@tanstack/query": require("@tanstack/eslint-plugin-query"),
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "react/no-unescaped-entities": "off",
+      "@tanstack/query/exhaustive-deps": "error",
+      "@tanstack/query/no-rest-destructuring": "warn",
+      "@tanstack/query/stable-query-client": "error",
+      "react/display-name": "off",
+    },
+  },
 ];
-
-export default eslintConfig;
