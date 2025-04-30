33b62d61562eaccb42874935861995dab8496a5c
Vini Black <viniibl4ck@gmail.com>
2025-04-30 17:27:23 -0300

    updating banner

--- src/components/features/banner/banner.tsx
+++ src/components/features/banner/banner.tsx
@@ -3,16 +3,21 @@ import { Flex } from '@/components/ui/flex'
 
 export const Banner = () => {
   return (
-    <Flex
-      className="relative w-full h-screen bg-[url(/images/banner.png)] md:bg-[url(/images/banner-desk.png)] bg-cover bg-center"
+    <section
+      className="relative h-[80vh] flex items-center justify-center bg-cover bg-center bg-[url(/images/banner.png)] md:bg-[url(/images/banner-desk.png)]"
     >
-      <div className="absolute inset-0 bg-black/60 z-0" />
-
-      <div className="relative z-5 flex flex-col justify-center ms-4 text-white">
-        <h2 className='text-[5rem] font-bold'>Rentall</h2>
-        <span className='text-[1rem]'>Incredible deals. Top-of-the-line design for less</span>
-        <Button className='mt-4 w-40'>See our big sale</Button>
+      <div className="absolute inset-0 bg-black/50" />
+      <div className="relative z-5 flex flex-col justify-center text-center items-center text-white max-w-3xl px-4" style={{ margin: "0 auto" }}>
+        <h1 className="text-4xl md:text-5xl font-extrabold leading-tight">
+          Próximo <span className="text-emerald-400">destino</span><br /> é com <span className="text-emerald-400">Rentall.</span>
+        </h1>
+        <p className="mt-4 text-lg md:text-xl">
+          Alugue ou disponibilize equipamentos e viva novas experiências.
+        </p>
+        <button className="mt-6 px-6 py-3 w-50 bg-emerald-600 hover:bg-emerald-700 rounded-full text-white font-semibold transition">
+          Comece agora
+        </button>
       </div>
-    </Flex>
+    </section>
   )
 }
